c3cbc4d13301893a5b07310f71f23c7ef59e4471
Zoya Musharraf <musharra@usc.edu>
2025-11-12 23:25:00 +0000

    part 1 done

--- equal-paths.cpp
+++ equal-paths.cpp
@@ -1,5 +1,6 @@
 #ifndef RECCHECK
 //if you want to add any #includes like <iostream> you must do them here (before the next endif)
+#include <iostream>
 
 #endif
 
@@ -8,11 +9,37 @@ using namespace std;
 
 
 // You may add any prototypes of helper functions here
+int depthOrFail(Node* n)
+{
+  if(n == nullptr){
+    return 0; //empty subtree
+  }
 
+  if(n->left == nullptr && n->right == nullptr){
+    return 1; //leaf depth 1
+  }
+
+  int leftDepth = depthOrFail(n->left);
+  int rightDepth = depthOrFail(n->right);
+
+  if(leftDepth == -1 || rightDepth == -1){
+    return -1;
+  }
+
+  //both children exist and have different depths
+  if(n->left != nullptr && n->right != nullptr && leftDepth != rightDepth){
+    return -1;
+  }
+
+  if(leftDepth != 0){
+    return leftDepth + 1;
+  } else {
+    return rightDepth + 1;
+  }
+}
 
 bool equalPaths(Node * root)
 {
     // Add your code below
-
+    return depthOrFail(root) != -1;
 }
-
