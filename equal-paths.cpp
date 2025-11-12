fe33844353e32c82012d8d3634d66e401e12af95
Zoya Musharraf <musharra@usc.edu>
2025-11-12 04:31:13 +0000

    Initial commit for hw4

---
+++ equal-paths.cpp
@@ -0,0 +1,18 @@
+#ifndef RECCHECK
+//if you want to add any #includes like <iostream> you must do them here (before the next endif)
+
+#endif
+
+#include "equal-paths.h"
+using namespace std;
+
+
+// You may add any prototypes of helper functions here
+
+
+bool equalPaths(Node * root)
+{
+    // Add your code below
+
+}
+
